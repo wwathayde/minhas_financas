0cc70bd776304c07cf51487e7c9dc0bd51280f9a
Wesley Athayde <wwathayde@gmail.com>
2022-10-14 09:26:09 -0300

    Adicionado anotação CrossOrigin

--- src/main/java/com/wsys/financas/controller/UsuarioController.java
+++ src/main/java/com/wsys/financas/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import com.wsys.financas.service.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@CrossOrigin
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/usuarios")
